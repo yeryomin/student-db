d8441c9f09b1d717d0fdfbd6ef25fb6c5b7094e0
Roman Yeryomin <roman@advem.lv>
2024-12-09 11:03:43 +0200

    main: exit after reading data
    Rationale is that we don't want to add anything after reading data.
    
    Signed-off-by: Roman Yeryomin <roman@advem.lv>

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main() {
 	struct student *data;
 	int err = 0;
 
-	printf("Do you want to see existing student data?\n");
+	printf("Do you want to see existing student data?(1/0)\n");
 	scanf("%d", &yn);
 	if ( yn == 1 ) {
 		printf("Reading %s\n", file);
@@ -29,6 +29,8 @@ int main() {
 			printf("There are %d students in database:\n", c);
 			data_output( c, data );
 		}
+
+		return 0;
 	}
 
 	printf("Please enter number of students: ");
