2166f0a0f047e47e0ccfa7e2d84ba95f9f2e25dd
Roman Yeryomin <roman@advem.lv>
2024-12-20 13:47:17 +0200

    main: add newline, because scanf is broken
    Signed-off-by: Roman Yeryomin <roman@advem.lv>

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main() {
 		return 0;
 	}
 
-	printf("Please enter number of students: ");
+	printf("Please enter number of students:\n");
 	scanf("%d", &c);
 
 	s = (struct student *)malloc( sizeof(struct student) * c );
@@ -41,7 +41,7 @@ int main() {
 	data_input( c, s );
 	data_output( c, s );
 
-	printf("Do you want to save the data(1/0)?");
+	printf("Do you want to save the data(1/0)?\n");
 	scanf("%d", &yn);
 	if ( yn == 1 ) {
 		printf("Saving data to %s ...\n", file);
